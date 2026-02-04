19fa731ef44e94202144f750f4a1a80a0f1f8cf0
Hojiakbar <hhgta4146@gmail.com>
2026-02-04 18:19:15 +0500

    Fix database regression: Force PostgreSQL backend by default

--- backend/inventory_system/settings.py
+++ backend/inventory_system/settings.py
@@ -81,12 +81,12 @@ WSGI_APPLICATION = 'inventory_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': config('DB_ENGINE', default='django.db.backends.sqlite3'),
-        'NAME': config('DB_NAME', default=str(BASE_DIR / 'db.sqlite3')),
-        'USER': config('DB_USER', default=''),
+        'ENGINE': config('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': config('DB_NAME', default='inventory_db'),
+        'USER': config('DB_USER', default='inventory_user'),
         'PASSWORD': config('DB_PASSWORD', default=''),
-        'HOST': config('DB_HOST', default=''),
-        'PORT': config('DB_PORT', default=''),
+        'HOST': config('DB_HOST', default='db'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
